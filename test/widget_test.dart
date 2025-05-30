a7d256a662e77c4b44c49f57850f82a995ff7322
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-05-30 14:08:42 +0900

    commit 20250530 step4 & cd/ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,12 @@
 // test/widget_test.dart
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pigeon_calendar_sample/main.dart'; // あなたのエントリーポイント
+import 'package:pigeon_calendar_sample/main.dart'; // あなたの main.dart のパスに合わせる
 
 void main() {
-  testWidgets('App starts without errors', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+  testWidgets('アプリが MyApp を一つ描画する', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    // MyApp（MaterialAppなど）を一つだけ描画しているか
     expect(find.byType(MyApp), findsOneWidget);
   });
 }
